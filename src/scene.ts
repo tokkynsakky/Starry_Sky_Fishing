ec5b495f7c087da2484ae20a0821c1e29e4ca519
tokkynsakky <sakiyamatokio@icloud.com>
2023-12-19 14:56:01 +0900

    Revert "Merge branch 'test-brach-tokkynsakky'"
    This reverts commit 66a6080ba692c5b2dd7e96f0fd62bc14af7b1875, reversing
    changes made to 984fabdef141c5cbda749b5f805fd0c8ecbcf430.

--- src/scene.ts
+++ src/scene.ts
@@ -1,7 +1,6 @@
-// scene.ts;
-
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
+// import useLogger from './logger';
+// const log = useLogger();
 
 export interface ARScene {
   makeObjectTree(): THREE.Object3D;
@@ -10,73 +9,58 @@ export interface ARScene {
   name(): string;
 }
 
-//   // GLTFLoaderを使用してrocket.gltfを読み込む
-//   const loader = new GLTFLoader();
-//   loader.load(
-//     'path/to/rocket.gltf',
-//     (gltf) => {
-//       this.rocket = gltf.scene;
-//       // ここでロケットの位置やサイズを設定することができます
-//       this.rocket.scale.set(0.1, 0.1, 0.1);
-//     },
-//     undefined,
-//     (error) => {
-//       console.error('Error loading rocket model', error);
-//     }
-//   );
-
-//   return new THREE.Object3D(); // 空のObject3Dを返すか、ロケットの3Dモデルが読み込まれるまで待つ処理が必要です
-// }
-
 export class TestScene implements ARScene {
   cube?: THREE.Object3D;
-  rocket?: THREE.Object3D;
-  isLaunch?: boolean;
-  passedTime?: number;
 
   name() {
     return "test";
   }
   makeObjectTree(): THREE.Object3D {
-    this.isLaunch = false;
-    this.passedTime = 0;
-    const loader = new GLTFLoader();
-    loader.load("/rocket.gltf", (gltf) => {
-      this.rocket = gltf.scene;
-      this.rocket.position.y = 0;
-      // this.scene.add(rocket);
-    });
-
     // log.info("make object tree", this.name())
-    // const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1).translate(0, 0.05, 0);
-    // const material = new THREE.MeshPhongMaterial({
-    // color: 0xffffff * Math.random(),
-    // });
-    // const cube = new THREE.Mesh(geometry, material);
-    this.cube = this.rocket;
+    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1).translate(0, 0.05, 0);
 
-    if (this.rocket === undefined) {
-      alert("rocket is undefined");
-      throw new Error("rocket is undefined");
-    }
-    return this.rocket;
-  }
-
-  launch() {
-    this.isLaunch = true;
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff * Math.random(),
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    this.cube = cube;
+    return cube;
   }
 
   animate(sec: number): void {
-    if (!this.cube) alert("rocket cant animete");
     if (!this.cube) return;
-
-    if (this.isLaunch && this.passedTime !== undefined) {
-      this.cube.position.y += this.passedTime ** 2;
-      this.passedTime += 0.001;
-    }
 
     // 立方体を回転させるアニメーション
     //this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 1 * sec;
+  }
+}
+
+export class TestScene2 implements ARScene {
+  cube?: THREE.Object3D;
+
+  name() {
+    return "test2";
+  }
+
+  makeObjectTree(): THREE.Object3D {
+    // 立方体のジオメトリを作成
+    const geometry = new THREE.ConeGeometry(0.1, 0.1).translate(0, 0.05, 0);
+
+    // 材質を作成（色を指定）
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+
+    // 立方体のメッシュを作成
+    this.cube = new THREE.Mesh(geometry, material);
+
+    return this.cube;
+  }
+
+  animate(sec: number): void {
+    if (!this.cube) return;
+
+    // 立方体を回転させるアニメーション
+    //this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
   }
 }
